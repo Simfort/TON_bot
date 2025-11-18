fdbedc7d19cc65216d12a330d298f34481fb5cab
Simfort <lemondzavadavid265@gmail.com>
2025-11-18 22:50:03 +0300

    reorginased directory

--- bot/dist/commands/buy_ton.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const buy_ton = (ctx) => {
-    ctx.session.commandBuyTon = true;
-    return ctx
-        .answerCbQuery()
-        .then(() => ctx.reply("<b>Назовите число</b>", { parse_mode: "HTML" }));
-};
-exports.default = buy_ton;
